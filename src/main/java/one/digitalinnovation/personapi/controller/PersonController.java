47b7815c190ebe6083019756c4ccca305edf727a
Rafael De Luca <deluca1712@gmail.com>
2021-07-23 13:07:16 -0300

    Refactoring PersonController e PersonService

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,23 +1,38 @@
 package one.digitalinnovation.personapi.controller;
 
 import lombok.AllArgsConstructor;
+import one.digitalinnovation.personapi.dto.response.SucessMessageDTO;
 import one.digitalinnovation.personapi.entities.Person;
+import one.digitalinnovation.personapi.repositories.PersonRepository;
 import one.digitalinnovation.personapi.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController("/api/v1/person")
-@AllArgsConstructor(onConstructor = @__(@Autowired))
+@RestController
+@RequestMapping("api/v1/people")
 public class PersonController {
 
-    private PersonService personService;
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Person create(Person person) {
-        return personService.create(person);
+    public SucessMessageDTO createPerson(@RequestBody Person person) {
+        Person savedPerson = personRepository.save(person);
+
+        return SucessMessageDTO
+                .builder()
+                .message("Create person with this ID: " +savedPerson.getId())
+                .build();
     }
+
+
 }
--- src/main/java/one/digitalinnovation/personapi/services/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/services/PersonService.java
@@ -1,19 +1,31 @@
 package one.digitalinnovation.personapi.services;
 
 import lombok.AllArgsConstructor;
+import one.digitalinnovation.personapi.dto.response.SucessMessageDTO;
 import one.digitalinnovation.personapi.entities.Person;
 import one.digitalinnovation.personapi.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Service
-@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 
     private PersonRepository personRepository;
 
-    public Person create(Person person) {
-        return personRepository.save(person);
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
     }
+
+    public SucessMessageDTO createPerson(Person person) {
+        Person savedPerson = personRepository.save(person);
+
+        return SucessMessageDTO
+                .builder()
+                .message("Create person with this ID: " +savedPerson.getId())
+                .build();
+    }
+
 }
